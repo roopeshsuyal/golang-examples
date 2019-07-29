a9c6de91c8086f8f05b3344441b8191068dce902
Roopesh Suyal <roopeshsuyal@users.noreply.github.com>
2019-07-29 22:46:57 +0530

    Update main.go

--- examples/regex-and-md5/main.go
+++ examples/regex-and-md5/main.go
@@ -1,34 +1,61 @@
 package main
 
 import (
+	"crypto/md5"
+	"encoding/hex"
+	"errors"
 	"fmt"
-	"regexp"	
-        "crypto/md5"
-        "io"
-        "encoding/hex"
+	"io"
+	"net/url"
+	"regexp"
 )
 
 func main() {
 
-	url := "https://mydoamin.com/mypage.html?test=1&c=2"
-	matched, err := regexp.Match(`^https?:\/\/.*$`, []byte(url))
+	URL := "https://mydoamin.com/mysubpage/mypage.html?test=1&c=2"
+
+	urlPath, err := getURLPath(URL)
 	if err != nil {
 		fmt.Println(err)
-		return
 	}
 
-	if matched {
-		fmt.Println("valid")
-	} else {
-		fmt.Println("Invalid")
-	}
-	
-	fmt.Printf("%s",mymd5("i am a string"))
+	fmt.Println(urlPath)
+
+	fmt.Println("===============MD5============")
+
+	fmt.Printf("%s", mymd5("i am a string"))
 	fmt.Println(" ")
 	fmt.Println(mymd5("i am a string"))
 
 }
 
+func getURLPath(URL string) (string, error) {
+
+	matched, err := validateHTTPURL(URL)
+	if err != nil {
+		return "", err
+	}
+
+	if !matched {
+		return "", errors.New(URL + " is not valid url")
+	}
+
+	u, err := url.ParseRequestURI(URL)
+	if err != nil {
+		return "", err
+	}
+
+	return u.Path, nil
+}
+
+func validateHTTPURL(URL string) (bool, error) {
+
+	matched, err := regexp.Match(`^https?:\/\/.*$`, []byte(URL))
+	if err != nil {
+		return false, err
+	}
+	return matched, nil
+}
 
 func mymd5(str string) string {
 	h := md5.New()
